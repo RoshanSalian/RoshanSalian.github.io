a049622a12bd32f3f2f7080ba5b3e90418f8f079
Roshan S <roshansalian1997@gmail.com>
2022-12-04 21:26:38 +0530

    added dynamic age counter

---
+++ script/page.js
@@ -0,0 +1,17 @@
+const resultElement = document.getElementById('result')
+
+const currentDate = new Date()
+
+const birthdate = '05/07/1997';
+
+const birthDate = new Date(birthdate)
+
+const ageInMilliseconds = currentDate - birthDate
+
+let ageInYears = ageInMilliseconds / (365 * 24 * 60 * 60 * 1000)
+
+setInterval(() => {
+    ageInYears += 1 / (365 * 24 * 60 * 60)
+    // console.log(ageInYears.toFixed(10))
+    resultElement.innerHTML = ageInYears.toFixed(10)
+}, 100)
